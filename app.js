a6234b17beb338e24b87d2383a3acbfb8072b48c
skystrike005 <sebastian.kevin05@gmail.com>
2016-02-04 10:28:03 +0000

    Added register function

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ var express = require("express"),
     app = express(),
     bodyParser = require("body-parser"),
     methodOverride = require("method-override"),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local"),
     TodoList = require("./models/todo"),
+    User = require("./models/user"),
     mongoose = require("mongoose");
 
 // To specify the static resources, use app.use(express.static("XX")) Command
@@ -15,12 +18,29 @@ app.use(methodOverride());
 // To connect to the mongoDB 
 mongoose.connect("mongodb://localhost/todo_list");
 
+// Passport setup
+app.use(require("express-session")({
+    secret: "Once again Rusty wins cutest dog!",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    next();
+});
 //SETUP Routes
 
 var todoRoutes = require("./routes/todos");
+var indexRoutes = require("./routes/index");
 
 app.use("/", todoRoutes);
-
+app.use("/", indexRoutes);
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Todo List Started");
 });
---
+++ routes/index.js
@@ -0,0 +1,33 @@
+var express = require("express"),
+    router = express(),
+    passport = require("passport"),
+    User = require("../models/user");
+    
+router.get("/register", function(req, res){
+    res.render("register");
+});
+
+router.post("/register", function(req, res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+            console.log(user);
+           res.redirect("/"); 
+        });
+    });
+});
+
+router.get("/login", function(req, res){
+    res.render("login");
+});
+
+router.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/login");
+});
+
+module.exports = router;
