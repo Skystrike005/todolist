13b5ca08d5863a136b2fa94c3ec25acecdfa2c76
skystrike005 <sebastian.kevin05@gmail.com>
2016-02-04 05:35:12 +0000

    Separated todo route

--- app.js
+++ app.js
@@ -15,50 +15,11 @@ app.use(methodOverride());
 // To connect to the mongoDB 
 mongoose.connect("mongodb://localhost/todo_list");
 
-//Index Route
-app.get("/", function(req, res){
-    TodoList.find({}, function(err, todos){
-        if(err){
-            console.log(err);
-        } else {
-            res.render("index", {todos: todos});
-        }
-    })
-})
+//SETUP Routes
 
-//Create Route
-app.post("/new", function(req, res){
-    var newTask = {content: req.body.task};
-    TodoList.create(newTask, function(err, newCreate){
-        if(err){
-            console.log(err);
-        } else {
-            res.redirect("/");
-        }
-    });
-})
-//UPDATE status ROUTE
-app.post("/:id/:status", function(req,res){
-    TodoList.update({_id:req.params.id},{$set:{completed:req.params.status}},function(err, update){
-        if(err){
-            console.log(err);
-        } else {
-            console.log(update);
-        }
-    });
-});
+var todoRoutes = require("./routes/todos");
 
-//DELETE ROUTE
-app.delete("/:id", function(req, res){
-    console.log(req.params.id);
-    TodoList.findByIdAndRemove(req.params.id, function(err, deleted){
-        if(err){
-            console.log(err);
-        } else {
-            console.log(deleted);
-        }
-    });
-})
+app.use("/", todoRoutes);
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Todo List Started");
---
+++ routes/todos.js
@@ -0,0 +1,51 @@
+var express = require("express"),
+    router = express.Router(),
+    TodoList = require("../models/todo");
+    
+
+//Index Route
+router.get("/", function(req, res){
+    TodoList.find({}, function(err, todos){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("index", {todos: todos});
+        }
+    })
+})
+
+//Create Route
+router.post("/new", function(req, res){
+    var newTask = {content: req.body.task};
+    TodoList.create(newTask, function(err, newCreate){
+        if(err){
+            console.log(err);
+        } else {
+            res.redirect("/");
+        }
+    });
+})
+//UPDATE status ROUTE
+router.post("/:id/:status", function(req,res){
+    TodoList.update({_id:req.params.id},{$set:{completed:req.params.status}},function(err, update){
+        if(err){
+            console.log(err);
+        } else {
+            console.log(update);
+        }
+    });
+});
+
+//DELETE ROUTE
+router.delete("/:id", function(req, res){
+    console.log(req.params.id);
+    TodoList.findByIdAndRemove(req.params.id, function(err, deleted){
+        if(err){
+            console.log(err);
+        } else {
+            console.log(deleted);
+        }
+    });
+});
+
+module.exports = router;
