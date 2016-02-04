54103c554946c12e05129dbae3bf62c38af62421
skystrike005 <sebastian.kevin05@gmail.com>
2016-02-04 09:58:53 +0000

    revert

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ var express = require("express"),
     app = express(),
     bodyParser = require("body-parser"),
     methodOverride = require("method-override"),
-    passport = require("passport"),
-    LocalStrategy = require("passport-local"),
-    User = require("./models/user"),
+    TodoList = require("./models/todo"),
     mongoose = require("mongoose");
 
 // To specify the static resources, use app.use(express.static("XX")) Command
@@ -18,28 +16,10 @@ app.use(methodOverride());
 mongoose.connect("mongodb://localhost/todo_list");
 
 //SETUP Routes
-var todoRoutes = require("./routes/todos"),
-    indexRoutes = require("./routes/index");
 
-//Passport config
-app.use(require("express-session")({
-    secret: "Not another todo app",
-    resave: false,
-    saveUninitialized: false
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-
-app.use(function(req, res, next){
-    res.locals.currentUser = req.user;
-    next();
-});
+var todoRoutes = require("./routes/todos");
 
 app.use("/", todoRoutes);
-app.use("/", indexRoutes);
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Todo List Started");
